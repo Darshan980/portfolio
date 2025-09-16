9b79fa1003d170b566d291c2c3164d550aab4500
Darshan S <149801799+Darshan980@users.noreply.github.com>
2025-09-16 12:41:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Portfolio from './pages/Projects';
+import Projects from './pages/Projects';
 import Resume from './pages/Resume';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
