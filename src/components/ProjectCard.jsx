acf85b4e924219381b1668576c5a877b09d16a91
Darshan S <149801799+Darshan980@users.noreply.github.com>
2025-09-16 10:37:43 +0530

    Update ProjectCard.jsx

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,18 +3,27 @@ import './ProjectCard.css';
 
 function ProjectCard({ project }) {
   return (
-<div className="project-card">
-  <div className="project-image-container">
-    <img src={project.image} alt={project.title} className="project-image" />
-  </div>
-  <div className="project-info">
-    <h3>{project.title}</h3>
-    <p>{project.description}</p>
-    <a href={project.link} target="_blank" rel="noopener noreferrer" className="project-link">
-      View Project
-    </a>
-  </div>
-</div>
+    <div className="project-card">
+      <div className="project-image-container">
+        <img
+          src={project.image}
+          alt={project.title}
+          className="project-image"
+        />
+      </div>
+      <div className="project-info">
+        <h3>{project.title}</h3>
+        <p>{project.description}</p>
+        <a
+          href={project.link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="project-link"
+        >
+          View Project
+        </a>
+      </div>
+    </div>
   );
 }
 
