32ba930014dd5383029dab7ca73c60c4c4dc6173
Darshan S <149801799+Darshan980@users.noreply.github.com>
2025-09-30 17:55:20 +0530

    Update Resume.jsx

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
-import 'react-pdf/dist/esm/Page/TextLayer.css';
 import "./Resume.css";
 
 // Set worker to match react-pdf version
